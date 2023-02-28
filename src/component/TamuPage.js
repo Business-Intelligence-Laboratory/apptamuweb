1ff8d7756c334503212f7c7ff3effbd581fa2e4d
Arif Roska Perdana <73458415+arifroska@users.noreply.github.com>
2023-02-28 18:57:58 +0700

    feat : save data, popup, backdrop

--- src/component/TamuPage.js
+++ src/component/TamuPage.js
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React, { useState } from 'react';
 import Button from '@mui/material/Button';
 import CssBaseline from '@mui/material/CssBaseline';
 import TextField from '@mui/material/TextField';
@@ -14,6 +14,10 @@ import RadioGroup from '@mui/material/RadioGroup';
 import FormControl from '@mui/material/FormControl';
 import FormLabel from '@mui/material/FormLabel';
 import logoLab from '../assets/images/Logo LBI.png';
+import axios from 'axios';
+import Snackbar from '@mui/material/Snackbar';
+import MuiAlert from '@mui/material/Alert';
+import { Backdrop, CircularProgress } from '@mui/material';
 
 function Copyright(props) {
   return (
@@ -30,20 +34,69 @@ function Copyright(props) {
 
 const theme = createTheme();
 
+const Alert = React.forwardRef(function Alert(props, ref) {
+  return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+});
+
 export default function TamuPage() {
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    const data = new FormData(event.currentTarget);
-    console.log({
-      email: data.get('email'),
-      password: data.get('password'),
-    });
+  const [open, setOpen] = React.useState(false);
+  const [message, setMessage] = React.useState("");
+  const [type, setType] = React.useState("success");
+  const [showBackdrop, setShowBackDrop] = React.useState(false);
+
+  const handleClose = (event, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+
+    setOpen(false);
   };
 
-  const instansi = [{label: 'Sistem Informasi UNAND'},{label: 'Teknik Komputer UNAND'}, {label: 'Informatika UNAND'}, {label: 'Lainnya'}]
+  function showMessage(type, msg) {
+    setOpen(true)
+    setType(type)
+    setMessage(msg)
+  }
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    const data = new FormData(event.currentTarget);
+    console.log("sdasdas", event.currentTarget)
+    console.log("data", data)
+    // console.log(data)
+    // console.log({
+    //   nama: data.get('nama'),
+    //   noIdentitas: data.get('noIdentitas'),
+    //   listIntansi: data.get('listInstansi'),
+    //   jenisKelamin: data.get('jenisKelamin'),
+    //   statusKunjungan: data.get('statusKunjungan'),
+    // });
+    try {
+      setShowBackDrop(true)
+      const res = await axios.post('http://localhost:1208/saveTamu', data, { headers: { "Content-Type": "application/json" } })
+      // console.log(res.data);
+      showMessage("success", "Selamat Datang")
+      setShowBackDrop(false)
+      event.target.reset();
+    } catch (error) {
+      setShowBackDrop(true)
+      showMessage("error", error.message)
+      setShowBackDrop(false)
+      event.target.reset()
+    }
+  };
+
+  const instansi = [
+    { label: 'Sistem Informasi UNAND', value: 'Sistem Informasi UNAND' },
+    { label: 'Teknik Komputer UNAND', value: 'Teknik Komputer UNAND' },
+    { label: 'Informatika UNAND', value: 'Informatika UNAND' },
+    { label: 'Lainnya', value: 'Lainnya' }]
+
 
   return (
     <ThemeProvider theme={theme}>
+
       <Container component="main" maxWidth="xs">
         <CssBaseline />
         <Box
@@ -54,7 +107,7 @@ export default function TamuPage() {
             alignItems: 'center',
           }}
         >
-          <img src={logoLab} alt='Logo LBI' width={100} height={100}/>
+          <img src={logoLab} alt='Logo LBI' width={100} height={100} />
           <Typography component="h1" variant="h5" align='center'>
             Selamat Datang di Laboratory of Business Intelligence
           </Typography>
@@ -73,28 +126,29 @@ export default function TamuPage() {
               margin="dense"
               required
               fullWidth
-              name="noidentitas"
+              name="noIdentitas"
               label="No.Identitas / NIM / NIP"
-              id="noidentitas"
+              id="noIdentitas"
               autoComplete="current-password"
             />
             <Autocomplete
               disablePortal
               margin="dense"
               required
-              id="instansi"
+              id="listInstansi"
+              // name="listInstansi"
               options={instansi}
               fullWidth
-              renderInput={(params) => <TextField margin='dense' {...params} label="Instansi" />}
+              renderInput={(params) => <TextField name='instansi' margin='dense' variant='outlined' {...params} label="Instansi" />}
             />
             <FormControl margin="dense">
-              <FormLabel id="demo-row-radio-buttons-group-label">Jenis Kelamin</FormLabel>
+              <FormLabel id="jenisKelamin">Jenis Kelamin</FormLabel>
               <RadioGroup
                 row
                 required
                 aria-labelledby="demo-row-radio-buttons-group-label"
                 defaultValue="Laki-Laki"
-                name="row-radio-buttons-group"
+                name="jenisKelamin"
               >
                 <FormControlLabel value="Laki-Laki" control={<Radio />} label="Laki-Laki" />
                 <FormControlLabel value="Perempuan" control={<Radio />} label="Perempuan" />
@@ -106,7 +160,7 @@ export default function TamuPage() {
               fullWidth
               name="statusKunjungan"
               label="Tujuan Kunjungan"
-              id="statuskunjungan"
+              id="statusKunjungan"
               autoComplete="current-password"
             />
             <Button
@@ -121,6 +175,18 @@ export default function TamuPage() {
         </Box>
         <Copyright sx={{ mt: 8, mb: 4 }} />
       </Container>
+      <Backdrop
+        sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
+        open={showBackdrop}
+
+      >
+        <CircularProgress color="inherit" />
+      </Backdrop>
+      <Snackbar open={open} autoHideDuration={3000} onClose={handleClose}>
+        <Alert onClose={handleClose} severity={type} sx={{ width: '100%' }}>
+          {message}
+        </Alert>
+      </Snackbar>
     </ThemeProvider>
   );
 }
